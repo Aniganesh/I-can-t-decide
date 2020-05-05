6bc76221ac38c8e686932e814ddbcbff33768d07
aniruddha <aniganesh741@gmail.com>
2020-05-05 11:44:48 +0530

    Added animations, settings for disabling and enabling animations

--- script.js
+++ script.js
@@ -4,17 +4,30 @@ const taskList = document.querySelector("#task-list");
 const taskTextInput = document.querySelector("#task-text");
 const deciderButton = document.querySelector("#select-task");
 const mainCard = document.querySelector(".main-card");
+const animationsActivated = document.querySelector("#animation-checkbox").checked;
 let numberOfTasks = 0;
 let decisionTaken = false;
 
+function getRandomColourHex() {
+  let R = Math.floor(Math.random() * 256).toString(16);
+  R.length === 2 ? R = R : R = "0" + R;
+  let G = Math.floor(Math.random() * 256).toString(16);
+  G.length === 2 ? G = G : G = "0" + G;
+  let B = Math.floor(Math.random() * 256).toString(16);
+  B.length === 2 ? B = B : B = "0" + B;
+  const hex = "#" + R + G + B;
+  return hex;
+}
+
+function sleep(duration) {
+  return new Promise(function (resolve) { setTimeout(resolve, duration) })
+}
 
 const handleInputFocused = () => {
   if (decisionTaken) {
     const previousSelectedTask = taskList.querySelector(".selected-task");
     previousSelectedTask.classList.remove("selected-task");
-    console.log("Done");
   }else{
-    console.log("No decisionTaken");
   }
 };
 
@@ -34,12 +47,31 @@ const addTask = () => {
   }
 };
 
-const selectTask = () => {
+const selectTask = async () => {
   if (decisionTaken) {
     const previousSelectedTask = taskList.querySelector(".selected-task");
     previousSelectedTask.classList.remove("selected-task");
   }
   const tasks = taskList.querySelectorAll(".task");
+  if(animationsActivated){
+    let pointer = 0;
+    const numRolls = Math.floor(Math.random() * 5);
+    let rolled = 0;
+    while(rolled < numRolls){
+      tasks[pointer].classList.add(".highlighted");
+      tasks[pointer].style.setProperty("background", String(getRandomColourHex()));
+      await sleep(100);
+      tasks[pointer].classList.remove(".highlighted");
+      tasks[pointer].style.setProperty("background", "");
+      ++pointer;
+      if(pointer >= numberOfTasks){
+        pointer = 0;
+        ++rolled;
+      }
+
+    }
+  }
+  
   const selected = Math.floor(Math.random() * numberOfTasks);
   tasks[selected].classList.add("selected-task");
   decisionTaken = true;
@@ -48,8 +80,12 @@ const selectTask = () => {
 const deleteTask = (event) => {
   if(event.target.classList.contains("delete-task")){    
     taskList.removeChild(event.target.parentElement);
+    --numberOfTasks;
   }
 }
+
+
+// Event Listeners
 
 mainCard.addEventListener("click", deleteTask);
 taskTextInput.addEventListener("focus", handleInputFocused);
